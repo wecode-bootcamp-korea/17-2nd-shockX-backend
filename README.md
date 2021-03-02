bff3ae3e099323b5b618650867d1bb529ceccb43
binogood <binogood68@gmai.com>
2021-03-02 15:36:03 +0900

    Add: 초기셋팅 완료

---
+++ README.md
@@ -0,0 +1,1 @@
+# 17-2st-shockx-backend
---
+++ my_settings.py
@@ -0,0 +1,14 @@
+DATABASES = {
+        'default' : {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': '200store',
+            'USER': 'root',
+            'PASSWORD': 'qwe123!!',
+            'HOST': 'localhost',
+            'PORT': '3306',
+        }
+    }
+SECRET_KEY = 'pa#mi^))us1(lgkj-sa4ij46l2#oax+8^n#9!hi4__)c(!-uk1'
+    
+
+ALGORITHM = 'HS512'
