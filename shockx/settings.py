115a2f9c6dc4e53ccc885e1e3e50dc9b149a7709
binogood <binogood68@gmai.com>
2021-03-03 18:27:55 +0900

    Add: create all models.py

--- shockx/settings.py
+++ shockx/settings.py
@@ -27,6 +27,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user',
+    'product',
+    'order',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -37,6 +41,7 @@ MIDDLEWARE = [
     #'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'shockx.urls'
---
+++ user/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+class User(models.Model):
+    email           = models.CharField(max_length=100, unique=True)
+    name            = models.CharField(max_length=50)
+    created_at      = models.DateTimeField(auto_now_add=True)
+    updated_at      = models.DateTimeField(auto_now=True)
+    seller_level    = models.ForeignKey('SellerLevel', on_delete=models.CASCADE, default=1)
+
+    class Meta:
+        db_table = 'users'
+
+class SellerLevel(models.Model):
+    name            = models.CharField(max_length=20)
+    transaction_fee = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        db_table = 'seller_levels'
+
+class ShippingInfomation(models.Model):
+    first_name        = models.CharField(max_length=50)
+    last_name         = models.CharField(max_length=50)
+    country           = models.CharField(max_length=50)
+    primary_address   = models.CharField(max_length=200)
+    secondary_address = models.CharField(max_length=200, null=True)
+    city              = models.CharField(max_length=45)
+    state             = models.CharField(max_length=45)
+    postal_code       = models.CharField(max_length=45)
+    phone_number      = models.CharField(max_length=45)
+    created_at        = models.DateTimeField(auto_now_add=True)
+    updated_at        = models.DateTimeField(auto_now=True)
+    user              = models.ForeignKey('User', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'shipping_infomations'
+
