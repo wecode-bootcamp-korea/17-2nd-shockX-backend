9b3a7184c38aec1f3acc7dc3061ac4c5563037fd
suasue <devsu7477@gmail.com>
2021-03-11 02:17:02 +0900

    Add: create portfolioview - create views.py, urls.py, tests.py

--- shockx/urls.py
+++ shockx/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('product', include('product.urls')),
-    path('order', include('order.urls'))
+    path('order', include('order.urls')),
+    path('user', include('user.urls'))
 ]
